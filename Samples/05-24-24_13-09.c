bab06847310b6a84a7c4d262321a6a980cf01e4d
John Cage <58808125+coshcage@users.noreply.github.com>
2024-05-24 20:18:25 +0000

    Add files via upload

---
+++ Samples/05-24-24_13-09.c
@@ -0,0 +1,160 @@
+/*
+ * Name:        05-24-24_13-09.c
+ * Description: A big integer calculator.
+ * Author:      cosh.cage#hotmail.com
+ * License:     GPLv3.
+ */
+#include <stdio.h>
+#include <string.h>
+#include "pbm.h"
+#define MAXLEN 100
+
+P_BINT CatchFirstNumber();
+P_BINT CalcBrackets();
+P_BINT CalcAddtional();
+P_BINT CalcAdvanced();
+P_BINT CalcPrimary();
+
+char * expr;
+
+P_BINT CalcPrimary()
+{
+	P_BINT x, y = NULL, r;
+	int c;
+	r = pbkCreateBint(0);
+	x = CalcAdvanced(expr);
+	c = expr[0];
+	while ('+' == c || '-' == c)
+	{
+		++expr;
+		y = CalcAdvanced(expr);
+		if ('+' == c)
+		{
+			pbkAddBint(r, x, y);
+			pbkMoveBint(x, r);
+		}
+		else if ('-' == c)
+		{
+			pbkSubtractBint(r, x, y);
+			pbkMoveBint(x, r);
+		}
+		c = expr[0];
+	}
+	if (expr[0] == ')')
+		++expr;
+	pbkDeleteBint(r);
+	if (NULL != y)
+		pbkDeleteBint(y);
+	return x;
+}
+
+P_BINT CalcAdvanced()
+{
+	P_BINT x, y = NULL, r, k;
+	int c;
+	r = pbkCreateBint(0);
+	k = pbkCreateBint(0);
+	x = CalcAddtional(expr);
+	c = expr[0];
+	while ('*' == c || '/' == c)
+	{
+		++expr;
+		y = CalcAdvanced(expr);
+		if ('*' == c)
+		{
+			pbkMultiplyBint(r, x, y);
+			pbkMoveBint(x, r);
+		}
+		else if (c == '/')
+		{
+			pbkDivideBint(r, k, x, y);
+			pbkMoveBint(x, r);
+		}
+		c = expr[0];
+	}
+	pbkDeleteBint(r);
+	pbkDeleteBint(k);
+	if (NULL != y)
+		pbkDeleteBint(y);
+	return x;
+}
+
+P_BINT CalcAddtional()
+{
+	P_BINT x, y, r;
+	r = pbkCreateBint(0);
+	x = CalcBrackets(expr);
+	if ('^' == expr[0])
+	{
+		_ub n;
+		++expr;
+		y = CalcAddtional(expr);
+		n = GETABS(pbkBintToIb(y));
+		pbmBintPower(r, x, n);
+		pbkDeleteBint(y);
+		pbkDeleteBint(x);
+		return r;
+	}
+	return x;
+}
+
+P_BINT CalcBrackets()
+{
+	if ('(' == expr[0])
+	{
+		++expr;
+		return CalcPrimary(expr);
+	}
+	else
+	{
+		return CatchFirstNumber(expr);
+	}
+}
+
+P_BINT CatchFirstNumber()
+{
+	size_t i;
+	P_BINT r;
+	P_BNUM t;
+	char szT[2] = { 0 };
+	char szTar[MAXLEN] = { 0 };
+	r = pbkCreateBint(0);
+	t = pbkCreateBnum(10);
+	for (i = 0; expr[0] != 0 && i < MAXLEN; ++expr, ++i)
+	{
+		if (expr[0] >= '0' && expr[0] <= '9')
+		{
+			szT[0] = expr[0];
+			strcat(szTar, szT);
+		}
+		else
+		{
+			pbkDecimalSzToBnum(t, szTar);
+			pbkDecimalBnumToBint(r, t);
+			pbkDeleteBnum(t);
+			return r;
+		}
+	}
+	pbkDecimalSzToBnum(t, szTar);
+	pbkDecimalBnumToBint(r, t);
+	pbkDeleteBnum(t);
+	return r;
+}
+
+int main(int argc, char ** argv)
+{
+	char szExpr[MAXLEN] = { 0 };
+	while (EOF != scanf("%s", szExpr))
+	{
+		P_BINT r;
+		P_BNUM t;
+		expr = szExpr;
+		t = pbkCreateBnum(10);
+		r = CalcPrimary();
+		pbkBintToDecimalBnum(t, r);
+		printf("= ");
+		pbkPrintBnum(t);
+		pbkDeleteBnum(t);
+		printf("\n");
+	}
+}
