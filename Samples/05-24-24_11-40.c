de2f94e06442343da59d1ecfc4b8f028ce76b00a
John Cage <58808125+coshcage@users.noreply.github.com>
2024-05-24 18:51:09 +0000

    Add files via upload

---
+++ Samples/05-24-24_11-40.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <Windows.h>
+#include "pbm.h"
+
+int main()
+{
+	P_BINT pc;
+	P_BNUM p;
+	LARGE_INTEGER la, lb, lc;
+
+	pc = pbkCreateBint(0);
+	p = pbkCreateBnum(10);
+
+	QueryPerformanceFrequency(&lc);
+	QueryPerformanceCounter(&la);
+
+	pbmUbFactorial(pc, 2000); // 2000!
+
+	QueryPerformanceCounter(&lb);
+	printf("%lf\n", (double)(lb.QuadPart - la.QuadPart) / (double)lc.QuadPart);
+
+	pbkBintToDecimalBnum(p, pc);
+
+	pbkPrintBnum(p);
+
+	return 0;
+}
